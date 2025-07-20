9e629cef29f596225dd83df5bbc12244fc6d6411
rathna1999 <67374701+rathna1999@users.noreply.github.com>
2025-07-20 16:36:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/info")
+def srtechopsinfo():
+    return '<h1 style="color:violet;">****Welcome to SRTechOps DevOps Class---Thank You</h1>'
+
+@app.route("/contact")
+def srtechopsmobilenumber():
+    return  '<h1 style="color:red;">FOR TRAINING ENQUIRY: +91 9150477118</h1>'
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
